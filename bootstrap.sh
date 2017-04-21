5e6cbfd34eaf3a958dc6bfa491ed4c6cf38b3dec
vagrant <vagrant@vagrant.vm>
2017-04-21 07:42:19 +0000

    init

---
+++ bootstrap.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#installing grpc
+
+cd /vagrant
+sudo apt-get install build-essential autoconf libtool
+sudo apt-get install libgflags-dev libgtest-dev
+sudo apt-get install clang libc++-dev
+git clone -b $(curl -L http://grpc.io/release) \\
+https://github.com/grpc/grpc
+cd grpc
+git submodule update --init
+make && sudo make install
+cd third_party/protobuf
+sudo apt-get install unzip
+./autogen.sh
+./configure
+make && sudo make install
+cd -
+cd examples/cpp/helloworld/
+sudo apt-get install pkg-config
+make
+
+#installing hypre
+
+cd /vagrant
+sudo apt-get install mpich
+git clone https://github.com/LLNL/hypre
+cd hypre/src
+./configure
+make

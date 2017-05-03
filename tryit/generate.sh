60caac62e1121e6a179b880e1b13304c3804ca75
Thomas Rückert <thomas.rueckert@s2010.tu-chemnitz.de>
2017-05-03 08:33:37 +0200

    updated protoc-generate-script to create services, too

--- tryit/generate.sh
+++ tryit/generate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 protoc --proto_path=src/protos --cpp_out=src/cpp/ src/protos/hypre.proto
+protoc --proto_path=src/protos --grpc_out=src/cpp/ --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` src/protos/hypre.proto

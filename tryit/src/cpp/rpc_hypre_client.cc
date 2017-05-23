a63613a984469b28d990b15e9d7de953b7016e99
Thomas Rückert <thomas.rueckert@s2010.tu-chemnitz.de>
2017-05-23 15:05:15 +0200

    fixed setupSolver()-method to actually delete objects that are getting created, started implementing vector creation

--- tryit/src/cpp/rpc_hypre_client.cc
+++ tryit/src/cpp/rpc_hypre_client.cc
@@ -67,36 +67,20 @@ public:
   Status setupSolver() {
 
     //set it up
-
-    rpc_hypre::RPC_HYPRE_BoomerAMGSolveMessage* request = new rpc_hypre::RPC_HYPRE_BoomerAMGSolveMessage;
-
     rpc_hypre::RPC_HYPRE_ParCSRMatrix* matrixA = new rpc_hypre::RPC_HYPRE_ParCSRMatrix;
     rpc_hypre::RPC_HYPRE_ParVector* par_b = new rpc_hypre::RPC_HYPRE_ParVector;
     rpc_hypre::RPC_HYPRE_ParVector* par_x = new rpc_hypre::RPC_HYPRE_ParVector;
     rpc_hypre::RPC_HYPRE_Solver* solverMessage = new rpc_hypre::RPC_HYPRE_Solver;
-
     solverMessage->set_identifier(solverIdentifier);
     matrixA->set_identifier(matrixIdentifier);
 
-    request->set_allocated_solver(solverMessage);
-    request->set_allocated_parcsr_a(matrixA);
-    request->set_allocated_par_b(par_b);
-    request->set_allocated_par_x(par_x);
+    rpc_hypre::RPC_HYPRE_BoomerAMGSolveMessage request;
+    request.set_allocated_solver(solverMessage);
+    request.set_allocated_parcsr_a(matrixA);
+    request.set_allocated_par_b(par_b);
+    request.set_allocated_par_x(par_x);
 
-    ::rpc_hypre::Empty emptyResponse;
-
-    ClientContext context;
-    std::cout << "Running RPC_HYPRE_BoomerAMGSetup()." << std::endl;
-    Status status = stub_->RPC_HYPRE_BoomerAMGSetup(&context, *request, &emptyResponse);
-    std::cout << "Finished running RPC_HYPRE_BoomerAMGSetup()." << std::endl;
-
-    delete request;
-
-    //results in seg fault:
-//    delete matrixA;
-//    delete par_b;
-//    delete par_x;
-//    delete solver2;
+    Status status = stub_->RPC_HYPRE_BoomerAMGSetup((new ClientContext), request, (new ::rpc_hypre::Empty));
 
     return status;
 
@@ -143,19 +127,7 @@ public:
     int local_size, extra;
 
     int num_procs = 1;
-    int myid = 1;
-
-//    int solver_id;
-//    int print_system;
-
-//    double h, h2;
-
-//    HYPRE_IJMatrix A;
-//    HYPRE_ParCSRMatrix parcsr_A;
-//    HYPRE_IJVector b;
-//    HYPRE_ParVector par_b;
-//    HYPRE_IJVector x;
-//    HYPRE_ParVector par_x;
+    int myid = 0;
 
     /* Default problem parameters */
     n = 33;
@@ -163,20 +135,12 @@ public:
     /* Preliminaries: want at least one processor per row */
     if (n*n < num_procs) n = sqrt(num_procs) + 1;
     N = n*n; /* global number of rows */
-//    h = 1.0/(n+1); /* mesh size*/
-//    h2 = h*h;
-
-//    std::cout << "global number of rows: " << N << std::endl;
-//    std::cout << "mesh size: " << h << std::endl;
 
     /* Each processor knows only of its own rows - the range is denoted by ilower
        and upper.  Here we partition the rows. We account for the fact that
        N may not divide evenly by the number of processors. */
     local_size = N/num_procs;
     extra = N - local_size*num_procs;
-
-//    std::cout << "num procs: " << num_procs << std::endl;
-//    std::cout << "local size: " << local_size << std::endl;
 
     ilower = local_size*myid;
     ilower += hypre_min(myid, extra);
@@ -204,7 +168,6 @@ public:
     if (!status.ok()) {
       return status;
     }
-
     /* Choose a parallel csr format storage (see the User's Manual) */
     /// ----------------------------------- SET TYPE -----------------------------------
 
@@ -232,23 +195,14 @@ public:
     initRequest->set_identifier(matrix.identifier());
 
     status = stub_->RPC_HYPRE_IJMatrixInitialize(&initializeContext, *initRequest, &matrix);
+    if (!status.ok()) {
+      return status;
+    }
 
-    /* Now go through my local rows and set the matrix entries.
-       Each row has at most 5 entries. For example, if n=3:
-
-       A = [M -I 0; -I M -I; 0 -I M]
-       M = [4 -1 0; -1 4 -1; 0 -1 4]
-
-       Note that here we are setting one row at a time, though
-       one could set all the rows together (see the User's Manual).
-    */
     {
       int nnz;
       double values[5];
       int cols[5];
-
-//      std::cout << "ilower: " << ilower << std::endl;
-//      std::cout << "iupper: " << iupper << std::endl;
 
       for (i = ilower; i <= iupper; i++)
       {
@@ -300,6 +254,7 @@ public:
         ::rpc_hypre::RPC_HYPRE_IJMatrixSetValuesMessage* setValuesRequest =
             new ::rpc_hypre::RPC_HYPRE_IJMatrixSetValuesMessage;
 
+
         setValuesRequest->set_allocated_matrix(&matrix);
         setValuesRequest->set_nrows(1);
         setValuesRequest->add_ncols(nnz);
@@ -312,7 +267,10 @@ public:
 
         }
 
-        stub_->RPC_HYPRE_IJMatrixSetValues(&setValuesContext, *setValuesRequest, &matrix);
+        status = stub_->RPC_HYPRE_IJMatrixSetValues(&setValuesContext, *setValuesRequest, &matrix);
+        if (!status.ok()) {
+          return status;
+        }
 
       }
     }
@@ -323,21 +281,10 @@ public:
 //    HYPRE_IJMatrixAssemble(A);
 
     ClientContext assembleContext;
-    stub_->RPC_HYPRE_IJMatrixAssemble(&assembleContext, matrix, &matrix);
-
-    /* Note: for the testing of small problems, one may wish to read
-       in a matrix in IJ format (for the format, see the output files
-       from the -print_system option).
-       In this case, one would use the following routine:
-       HYPRE_IJMatrixRead( <filename>, MPI_COMM_WORLD,
-                           HYPRE_PARCSR, &A );
-       <filename>  = IJ.A.out to read in what has been printed out
-       by -print_system (processor numbers are omitted).
-       A call to HYPRE_IJMatrixRead is an *alternative* to the
-       following sequence of HYPRE_IJMatrix calls:
-       Create, SetObjectType, Initialize, SetValues, and Assemble
-    */
-
+    status = stub_->RPC_HYPRE_IJMatrixAssemble(&assembleContext, matrix, &matrix);
+    if (!status.ok()) {
+      return status;
+    }
 
     /* Get the parcsr matrix object to use */
     /// ----------------------------------- GET OBJECT -----------------------------------
@@ -346,60 +293,146 @@ public:
     ClientContext getObjectContext;
     ::rpc_hypre::RPC_HYPRE_GenericMatrixIntegerParam* getObjectRequest =
         new ::rpc_hypre::RPC_HYPRE_GenericMatrixIntegerParam;
-    stub_->RPC_HYPRE_IJMatrixGetObject(&getObjectContext, *getObjectRequest, &matrix);
+
+    getObjectRequest->set_allocated_matrix(&matrix);
+    status = stub_->RPC_HYPRE_IJMatrixGetObject(&getObjectContext, *getObjectRequest, &matrix);
+    if (!status.ok()) {
+      return status;
+    }
+
     matrixIdentifier = matrix.identifier();
 
-    /* Create the rhs and solution */
-
-    /// ----------------------------------- VECTOR -----------------------------------
-
-//    HYPRE_IJVectorCreate(MPI_COMM_WORLD, ilower, iupper,&b);
-//    HYPRE_IJVectorSetObjectType(b, HYPRE_PARCSR);
-//    HYPRE_IJVectorInitialize(b);
-//
-//    HYPRE_IJVectorCreate(MPI_COMM_WORLD, ilower, iupper,&x);
-//    HYPRE_IJVectorSetObjectType(x, HYPRE_PARCSR);
-//    HYPRE_IJVectorInitialize(x);
-//
-//    /* Set the rhs values to h^2 and the solution to zero */
-//    {
-//      double *rhs_values, *x_values;
-//      int    *rows;
-//
-//      rhs_values =  (double*) calloc(local_size, sizeof(double));
-//      x_values =  (double*) calloc(local_size, sizeof(double));
-//      rows = (int*) calloc(local_size, sizeof(int));
-//
-//      for (i=0; i<local_size; i++)
-//      {
-//        rhs_values[i] = h2;
-//        x_values[i] = 0.0;
-//        rows[i] = ilower + i;
-//      }
-//
-//      HYPRE_IJVectorSetValues(b, local_size, rows, rhs_values);
-//      HYPRE_IJVectorSetValues(x, local_size, rows, x_values);
-//
-//      free(x_values);
-//      free(rhs_values);
-//      free(rows);
-//    }
-//
-//
-//    HYPRE_IJVectorAssemble(b);
-//    /*  As with the matrix, for testing purposes, one may wish to read in a rhs:
-//        HYPRE_IJVectorRead( <filename>, MPI_COMM_WORLD,
-//                                  HYPRE_PARCSR, &b );
-//        as an alternative to the
-//        following sequence of HYPRE_IJVectors calls:
-//        Create, SetObjectType, Initialize, SetValues, and Assemble
-//    */
-//    HYPRE_IJVectorGetObject(b, (void **) &par_b);
-//
-//    HYPRE_IJVectorAssemble(x);
-//    HYPRE_IJVectorGetObject(x, (void **) &par_x);
-
     std::cout << "Finished matrix()" << std::endl;
+
+    return Status::OK;
+
+  }
+
+  Status setupVectors() {
+
+    int i;
+    int N, n;
+
+    int ilower, iupper;
+    int local_size, extra;
+
+    int num_procs = 1;
+    int myid = 0;
+
+    double h, h2;
+
+    Status status;
+    ::rpc_hypre::RPC_HYPRE_IJVector vector;
+
+    HYPRE_IJVector b;
+    HYPRE_ParVector par_b;
+    HYPRE_IJVector x;
+    HYPRE_ParVector par_x;
+
+    /* Default problem parameters */
+    n = 33;
+
+    /* Preliminaries: want at least one processor per row */
+    if (n*n < num_procs) n = sqrt(num_procs) + 1;
+    N = n*n; /* global number of rows */
+    h = 1.0/(n+1); /* mesh size*/
+    h2 = h*h;
+
+
+    /* Each processor knows only of its own rows - the range is denoted by ilower
+       and upper.  Here we partition the rows. We account for the fact that
+       N may not divide evenly by the number of processors. */
+    local_size = N/num_procs;
+    extra = N - local_size*num_procs;
+
+    ilower = local_size*myid;
+    ilower += hypre_min(myid, extra);
+
+    iupper = local_size*(myid+1);
+    iupper += hypre_min(myid+1, extra);
+    iupper = iupper - 1;
+
+    /* How many rows do I have? */
+    local_size = iupper - ilower + 1;
+
+    /* Create the rhs and solution */
+    /// ----------------------------------- CREATE -----------------------------------
+//    HYPRE_IJVectorCreate(MPI_COMM_WORLD, ilower, iupper,&b);
+
+    ::rpc_hypre::RPC_HYPRE_IJVectorCreateMessage createRequest;
+    createRequest.set_jlower(ilower);
+    createRequest.set_jupper(iupper);
+
+    status = stub_->RPC_HYPRE_IJVectorCreate((new ClientContext), createRequest, &vector);
+    if (!status.ok()) {
+      return status;
+    }
+
+    /// ----------------------------------- SET OBJECT -----------------------------------
+//    HYPRE_IJVectorSetObjectType(b, HYPRE_PARCSR);
+
+    ::rpc_hypre::RPC_HYPRE_GenericVectorxIntegerParam* setTypeRequest =
+        new ::rpc_hypre::RPC_HYPRE_GenericVectorxIntegerParam;
+    setTypeRequest->set_value(HYPRE_PARCSR);
+    setTypeRequest->set_allocated_vector(&vector);
+    status = stub_->RPC_HYPRE_IJVectorSetObjectType((new ClientContext), *setTypeRequest, &vector);
+
+        /// ----------------------------------- INIT -----------------------------------
+//    HYPRE_IJVectorInitialize(b);
+
+//    ::rpc_hypre::RPC_HYPRE_IJVector initRequest;
+//    initRequest.set_identifier(vector->)
+
+    status = stub_->RPC_HYPRE_IJVectorInitialize((new ClientContext), vector, &vector);
+
+    /// ----------------------------------- CREATE -----------------------------------
+//    HYPRE_IJVectorCreate(MPI_COMM_WORLD, ilower, iupper,&x);
+
+    /// ----------------------------------- SET OBJECT -----------------------------------
+//    HYPRE_IJVectorSetObjectType(x, HYPRE_PARCSR);
+
+    /// ----------------------------------- INIT -----------------------------------
+//    HYPRE_IJVectorInitialize(x);
+
+    /* Set the rhs values to h^2 and the solution to zero */
+    {
+      double *rhs_values, *x_values;
+      int    *rows;
+
+      rhs_values =  (double*) calloc(local_size, sizeof(double));
+      x_values =  (double*) calloc(local_size, sizeof(double));
+      rows = (int*) calloc(local_size, sizeof(int));
+
+      for (i=0; i<local_size; i++)
+      {
+        rhs_values[i] = h2;
+        x_values[i] = 0.0;
+        rows[i] = ilower + i;
+      }
+
+      /// ----------------------------------- SET VAL -----------------------------------
+//      HYPRE_IJVectorSetValues(b, local_size, rows, rhs_values);
+
+      /// ----------------------------------- SET VAL -----------------------------------
+//      HYPRE_IJVectorSetValues(x, local_size, rows, x_values);
+
+      free(x_values);
+      free(rhs_values);
+      free(rows);
+    }
+
+
+    /// ----------------------------------- ASSEMBLE -----------------------------------
+//    HYPRE_IJVectorAssemble(b);
+
+    /// ----------------------------------- GET OBJ -----------------------------------
+//    HYPRE_IJVectorGetObject(b, (void **) &par_b);
+
+    /// ----------------------------------- ASSEMBLE -----------------------------------
+//    HYPRE_IJVectorAssemble(x);
+
+    /// ----------------------------------- GET OBJ -----------------------------------
+//    HYPRE_IJVectorGetObject(x, (void **) &par_x);
 
     return Status::OK;
 
@@ -414,14 +447,12 @@ public:
   int executeExample5() {
 
     if (!checkStatus(createSolver())) return 0;
-
     if (!checkStatus(setupMatrix())) return 0;
-
+    if (!checkStatus(setupVectors())) return 0;
     if (!checkStatus(setupSolver())) return 0;
-
     if (!checkStatus(solve())) return 0;
-
     return 1;
+
   }
 
   int checkStatus(Status status) {
